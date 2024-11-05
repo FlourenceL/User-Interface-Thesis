5d585bbd88f8023a28761fe07bfabb94b80a20b9
Flourence Lapore <renzo.lapore@gmail.com>
2024-11-05 10:35:01 +0800

    Added charts

--- src/UserProgress.jsx
+++ src/UserProgress.jsx
@@ -5,6 +5,8 @@ import firebaseConfig from "./firebaseConfig";
 import {
   Bar,
   BarChart,
+  Line,
+  LineChart,
   ResponsiveContainer,
   XAxis,
   YAxis,
@@ -21,18 +23,23 @@ import {
   TableContainer,
   TableHead,
   TableRow,
-  Paper,
   Typography,
-  Box, // Import Box component from MUI
+  Paper,
+  Box,
+  IconButton,
 } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
 
 const UserProgress = () => {
   const [users, setUsers] = useState([]);
   const [currentUserProgress, setCurrentUserProgress] = useState([]);
   const [modal, setModal] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
 
   const openModal = (progress) => {
-    setCurrentUserProgress(progress); // Set the current user's progress data
+    setCurrentUserProgress(progress);
     setModal(true);
   };
 
@@ -51,8 +58,9 @@ const UserProgress = () => {
             (progressKey) => ({
               createdAt: new Date(
                 userProgress[progressKey].createdAt
-              ).toLocaleDateString(), // Convert timestamp to readable date
+              ).toLocaleDateString(),
               weight: userProgress[progressKey].weight,
+              height: userProgress[progressKey].height, // Fetch fat percentage
             })
           );
           return {
@@ -66,101 +74,143 @@ const UserProgress = () => {
     });
 
     return () => {
-      off(usersRef); // Cleanup listener when component unmounts
+      off(usersRef);
     };
   }, []);
 
-  //------------------------------------
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? "dark" : "light",
+    },
+  });
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
 
   return (
-    <Container maxWidth="sm">
-      <Typography variant="h4" gutterBottom>
-        Users
-      </Typography>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="sm">
+        <Paper elevation={3} sx={{ padding: 2, marginTop: 4 }}>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Typography variant="h4" gutterBottom>
+              Users
+            </Typography>
+            <IconButton onClick={toggleDarkMode} color="inherit">
+              {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+            </IconButton>
+          </Box>
 
-      {/* MUI Table */}
-      <TableContainer component={Paper}>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>
-                <strong>Name</strong>
-              </TableCell>
-              <TableCell>
-                <strong>Age</strong>
-              </TableCell>
-              <TableCell>
-                <strong>Action</strong>
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {users.map((user) => (
-              <TableRow key={user.id}>
-                <TableCell>
-                  {user.firstName} {user.lastName}
-                </TableCell>
-                <TableCell>{user.age}</TableCell>
-                <TableCell>
-                  <Button
-                    variant="contained"
-                    onClick={() => openModal(user.progress)}
-                  >
-                    View
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+          <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>
+                    <strong>Name</strong>
+                  </TableCell>
+                  <TableCell>
+                    <strong>Age</strong>
+                  </TableCell>
+                  <TableCell>
+                    <strong>Action</strong>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {users.map((user) => (
+                  <TableRow key={user.id}>
+                    <TableCell>
+                      {user.firstName} {user.lastName}
+                    </TableCell>
+                    <TableCell>{user.age}</TableCell>
+                    <TableCell>
+                      <Button
+                        variant="contained"
+                        onClick={() => openModal(user.progress)}
+                      >
+                        View
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
 
-      {/* Modal for User Progress */}
-      <Modal
-        open={modal}
-        onClose={closeModal}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box
-          className="box"
-          sx={{
-            padding: "20px",
-            backgroundColor: "#fff",
-            margin: "50px auto",
-            maxWidth: "600px",
-            borderRadius: "8px", // Rounded corners
-            boxShadow: 3, // Box shadow for depth effect
-          }}
+        {/* Modal for User Progress */}
+        <Modal
+          open={modal}
+          onClose={closeModal}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
         >
-          <ResponsiveContainer width="100%" height={400}>
-            <BarChart
-              data={currentUserProgress}
-              margin={{ top: 30, right: 30, left: 20, bottom: 5 }}
-            >
-              <XAxis dataKey="createdAt" />
+          <Paper
+            elevation={3}
+            sx={{
+              padding: "20px",
+              backgroundColor: "#fff",
+              margin: "50px auto",
+              maxWidth: "600px",
+              borderRadius: "8px",
+              boxShadow: 3,
+            }}
+          >
+            <ResponsiveContainer width="100%" height={400}>
+              <BarChart
+                data={currentUserProgress}
+                margin={{ top: 30, right: 30, left: 20, bottom: 5 }}
+              >
+                <XAxis dataKey="createdAt" />
+                <YAxis
+                  domain={[
+                    0,
+                    Math.max(
+                      ...currentUserProgress.map((data) => data.weight)
+                    ) + 20,
+                  ]}
+                />
+                <CartesianGrid />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="weight" fill="#0000FF" />
+              </BarChart>
+            </ResponsiveContainer>
 
-              {/* Calculate and set the Y-axis domain */}
-              <YAxis
-                domain={[
-                  0,
-                  Math.max(...currentUserProgress.map((data) => data.weight)) +
-                    20,
-                ]}
-              />
+            <ResponsiveContainer width="100%" height={400}>
+              <LineChart
+                data={currentUserProgress}
+                margin={{ top: 30, right: 30, left: 20, bottom: 5 }}
+              >
+                <XAxis dataKey="createdAt" />
+                <YAxis
+                  domain={[
+                    0,
+                    Math.max(
+                      ...currentUserProgress.map(
+                        (data) => parseFloat(data.fat) || 0
+                      )
+                    ) + 5,
+                  ]}
+                />
+                <CartesianGrid />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="height" stroke="#FF0000" />
+              </LineChart>
+            </ResponsiveContainer>
 
-              <CartesianGrid />
-              <Tooltip />
-              <Legend />
-              <Bar dataKey="weight" fill="#8884d8" />
-            </BarChart>
-          </ResponsiveContainer>
-          <Button variant="contained" onClick={closeModal} sx={{ mt: 2 }}>
-            Close
-          </Button>
-        </Box>
-      </Modal>
-    </Container>
+            <Button variant="contained" onClick={closeModal} sx={{ mt: 2 }}>
+              Close
+            </Button>
+          </Paper>
+        </Modal>
+      </Container>
+    </ThemeProvider>
   );
 };
 
